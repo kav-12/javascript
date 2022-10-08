24933a31f0d545d7da5513f6ac06524a69d1598e
kavita <kavitayadav64@gmail.com>
2022-10-08 13:45:44 +0530

    calculator file add

---
+++ calculator.js
@@ -0,0 +1,30 @@
+const kavita ={
+    firstName : "kavita",
+    lastName : "yadav",
+    role : "learner" ,
+    courseCount : 4,
+    getInfo : function(){
+        console.log(`
+        firstName is ${this.firstName}
+        and last name is ${this.lastName}
+        courseCount is ${this.courseCount}
+        and his role is ${this.role}`);
+    },
+};
+
+const parveen = {
+    firstName: "parveen",
+    lastName : "yadav",
+    role : "govt-employee"
+};
+
+//kavita.getInfo();
+//parveen.getinfo();
+
+//kavita.getInfo.bind(parveen)();
+//var parveeninfo=kavita.getInfo.bind(parveen);
+//parveeninfo();
+
+kavita.getInfo.call(parveen);
+
+kavita.getInfo();
